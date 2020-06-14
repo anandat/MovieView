5feab79429821ecb6037fd09454ec1b618dc4554
Atul Anand <atul.anand@haufe.com>
2020-06-14 14:53:00 +0530

    add
    add

--- lib/catalog/ui/my_catalog.dart
+++ lib/catalog/ui/my_catalog.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flappy_search_bar/flappy_search_bar.dart';
 import 'package:flutter/material.dart';
@@ -23,64 +24,81 @@ class _MovieState extends State<MovieUi> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocListener<CatalogBloc, CatalogState>(
+     //catalogBloc = BlocProvider.of<CatalogBloc>(context);
+
+    return BlocListener(
+
       listener: (Context, state) {
         if (state is MovieDataResponse) {
           movie = state.movieModel;
         }
       },
-      child: BlocBuilder<CatalogBloc, CatalogState>(
+      bloc: catalogBloc,
+      child: BlocBuilder(
+        bloc: catalogBloc,
         builder: (Context, State) {
           // int size = movie.data.length;
-          return Scaffold(
-              appBar: appbar(),
-              body: GridView.count(
-                crossAxisCount: 3,
-                //we can put orientation check as well for dynamic
-                children: List.generate(10, (index) {
-                  return Center(
-                      child: Padding(
-                    padding: EdgeInsets.all(16),
-                    child: Column(
-                      children: <Widget>[
-                       // SearchBar(),
-                        // Api is not good for search
-                        // on search io add debouncing algo to better rewsult and cost optimization
-                        getTile(movie, index)
-                      ],
-                    ),
-                  ));
-                }),
-              ));
+          return gridView(movie);
         },
       ),
-      bloc: catalogBloc,
+
     );
   }
 }
 
-getTile(MovieModel movie, int index) {
+Widget gridView(MovieModel movie){
+ return Scaffold(
+    //  resizeToAvoidBottomPadding: false,
+      appBar: appbar(),
+      body: GridView.count(
+        crossAxisCount: 3,
+        scrollDirection: Axis.vertical,
+        shrinkWrap: true,
+        //we can put orientation check as well for dynamic
+        children: List.generate(10, (index) {
+          return Container(
+            height: 100,
+            width: 20,
 
-  return Column(
-    children: <Widget>[
-      
-      Container(
-        height: 20,
-        width: 50,
-        child: CachedNetworkImage(
-        //cacheManager: ,
-          width: 40,
-          progressIndicatorBuilder: (context, url, progress) =>
-              CircularProgressIndicator(
-                value: progress.progress,
-              ),
-          imageUrl:
-          movie.data[index].poster != null ? movie.data[index].poster: "",
-          errorWidget: (context, url, error) => Icon(Icons.error),
-        ),
+            //padding: EdgeInsets.all(16),
+            child: Flexible(
+                flex: 3,
+                child: Column(
+                  children: <Widget>[
+                    // SearchBar(),
+                    // Api is not good for search
+                    // on search io add debouncing algo to better rewsult and cost optimization
+
+                    getTile(movie, index),
+                    AutoSizeText(movie.data[index].title != null
+                        ? movie.data[index].title
+                        : ""),
+                    AutoSizeText(movie.data[index].year != null
+                        ? movie.data[index].year
+                        : ""),
+                  ],
+                )),
+          );
+        }),
+      ));
+}
+
+getTile(MovieModel movie, int index) {
+  (index != null && index.toString().isEmpty) ? index : "";
+  return Container(
+    height: 20,
+    width: 70,
+    child: CachedNetworkImage(
+      //cacheManager: ,
+      //width: 40,
+      progressIndicatorBuilder: (context, url, progress) =>
+          CircularProgressIndicator(
+        value: progress.progress,
       ),
-      Text(movie.data[index].title != null ? movie.data[index].title : ""),
-    ],
+      imageUrl:
+          (movie.data[index].poster != null &&  movie.data[index].poster.isNotEmpty)? movie.data[index].poster : "",
+      errorWidget: (context, url, error) => Icon(Icons.error),
+    ),
   );
 }
 
